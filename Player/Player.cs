1d1c35ab7b4870c348a87222875f761734c8d14a
OlliDev <oliver.koenig.bi@gmail.com>
2022-12-18 11:35:47 +0100

    Add IbputManager and better player

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,66 +1,139 @@
 using System;
+using System.Reflection;
 using Godot;
 
 public partial class Player : CharacterBody2D
 {
-    [Export] private NodePath _killArea;
+    [Signal] public delegate void JumpedEventHandler(Player sender, bool highJump);
+    [Signal] public delegate void LandedEventHandler(Player sender);
 
-    [Export, ExportGroup("Movement")] public float speed = 300;
-    [Export] public float jumpVelocity = -400.0f;
-    [Export] public float gravityScale = 1;
+    public const float JumpLenienceTime = 0.1f;
 
-    public float gravity;
+    [Export] private AnimationPlayer anim;
+    [Export] private GPUParticles2D runParticles;
+    [Export] private LevelMap map;
 
-    public Area2D killArea;
+    [Export, ExportGroup("Movement")] public float jumpVelocity;
+    [Export] public float gravityScale = 1, jumpingGravityScale;
+    [Export(PropertyHint.Range, "0,1")] public float jumpCancelStrenght;
+    [Export] public float groundedAcceleration, airAcceleration;
+    [Export(PropertyHint.Range, "0,1")] public float groundDamping;
+    [Export(PropertyHint.Range, "0,1")] public float groundedStopDamping;
+    [Export(PropertyHint.Range, "0,1")] public float airDamping;
+    [Export] public Vector2 diveUpVelocity;
+    [Export] public Vector2 diveHorizontalVelocity;
+
+    public float baseGravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+    private Timer groundRememberTimer;
+    private bool isGrounded, isJumping;
+    private bool faceLeft;
+
+    public bool FaceLeft
+    {
+        get => faceLeft;
+        set
+        {
+            faceLeft = value;
+
+            if (value)
+            {
+                Scale = new(1, -1);
+                Rotation = Mathf.DegToRad(180);
+                return;
+            }
+
+            Scale = new(1, 1);
+            Rotation = 0;
+        }
+    }
+
+    public override void _EnterTree()
+    {
+        InputManager.JumpReleased += OnJumpReleased;
+    }
+
+    public override void _ExitTree()
+    {
+        InputManager.JumpReleased -= OnJumpReleased;
+    }
 
     public override void _Ready()
     {
-        killArea = GetNode<Area2D>(_killArea);
-
-        killArea.BodyEntered += (body) =>
-        {
-            GD.Print("Colllll");
-        };
-
-        killArea.BodyShapeEntered += (RID bodyRid, Node2D body, long bodyShapeIndex, long localShapeIndex) =>
-        {
-            GD.Print("Collllliii");
-        };
-
-        // Get the gravity from the project settings to be synced with RigidBody nodes.
-        gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle() * gravityScale;
-        GD.Print(GetClass());
+        AddChild(
+            groundRememberTimer = new()
+            {
+                WaitTime = JumpLenienceTime,
+                OneShot = true
+            });
     }
 
     public override void _PhysicsProcess(double delta)
     {
         Vector2 velocity = Velocity;
+        float horizontalInput = InputManager.GetPlayerHorizontalInput();
 
-        // Add the gravity.
-        if (!IsOnFloor())
-            velocity.y += gravity * (float)delta;
+        ApplyGravity();
+        MoveHorizontal();
+        HandleJumping();
+        Move();
+        UpdateIsGrounded();
 
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-            velocity.y = jumpVelocity;
-
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        if (direction != Vector2.Zero)
+        void Move()
         {
-            velocity.x = direction.x * speed;
-        }
-        else
-        {
-            velocity.x = Mathf.MoveToward(Velocity.x, 0, speed);
+            Velocity = velocity;
+
+            if (MoveAndSlide())
+                HandleCollisions();
         }
 
-        Velocity = velocity;
+        void ApplyGravity()
+        {
+            if (isGrounded)
+                return;
 
-        if (MoveAndSlide())
-            HandleCollisions();
+            if (isJumping)
+            {
+                velocity.y += baseGravity * jumpingGravityScale * (float)delta;
+                return;
+            }
 
+            velocity.y += baseGravity * gravityScale * (float)delta;
+        }
+
+        void MoveHorizontal()
+        {
+            if (horizontalInput != 0)
+                FaceLeft = horizontalInput < 0;
+
+            if (isGrounded)
+            {
+                velocity.x += horizontalInput * groundedAcceleration * (float)delta;
+                velocity.x *= Mathf.Pow(1f - (horizontalInput == 0 ? groundedStopDamping : groundDamping), (float)delta * 10f);
+                return;
+            }
+
+            velocity.x += InputManager.GetPlayerHorizontalInput() * airAcceleration * (float)delta;
+            velocity.x *= Mathf.Pow(1f - airDamping, (float)delta * 10f);
+        }
+
+        void HandleJumping()
+        {
+            if (InputManager.IsJumpBuffered && (isGrounded || (groundRememberTimer.TimeLeft != 0 && !groundRememberTimer.IsStopped())))
+            {
+                Jump(jumpVelocity, ref velocity);
+                return;
+            }
+
+            if (isGrounded)
+            {
+                velocity.y = 1;
+                isJumping = false;
+                return;
+            }
+
+            if (Velocity.y > 0) isJumping = false;
+        }
 
         void HandleCollisions()
         {
@@ -72,6 +145,59 @@ public partial class Player : CharacterBody2D
                     map.ProcessCollision(this, collision);
             }
         }
+
+        void UpdateIsGrounded()
+        {
+            if (IsOnFloor() != isGrounded)
+            {
+                isGrounded = IsOnFloor();
+
+                if (isGrounded) Land();
+                else LeaveGround();
+            }
+        }
+    }
+
+
+    private void OnJumpReleased()
+    {
+        if (!isJumping)
+            return;
+
+        Vector2 velocity = Velocity;
+        CancelJump(ref velocity);
+        Velocity = velocity;
+    }
+
+    private void Jump(float jumpVelocity, ref Vector2 velocity)
+    {
+        Emit.Jumped(this, false);
+        EmitSignal(SignalName.Jumped, this);
+
+        isJumping = true;
+        velocity.y = jumpVelocity;
+
+        InputManager.UseJumpBuffer();
+        groundRememberTimer.Stop();
+
+        if (!InputManager.IsHoldingJump)
+            CancelJump(ref velocity);
+    }
+
+    private void CancelJump(ref Vector2 velocity)
+    {
+        isJumping = false;
+        velocity.y *= 1f - jumpCancelStrenght;
+    }
+
+    private void LeaveGround()
+    {
+        groundRememberTimer.Start();
+    }
+
+    private void Land()
+    {
+        Emit.Landed(this);
     }
 
     public void Die()
---
+++ Singletons/InputManager.cs
@@ -0,0 +1,75 @@
+using System;
+using Godot;
+
+public partial class InputManager : Node
+{
+    const float JumpBufferTime = 0.15f;
+
+    public static InputManager Instance { get; private set; }
+
+    public static event Action JumpPressed, JumpReleased, PausePressed;
+
+
+    public static bool IsJumpBuffered => Instance.jumpBufferTimer.TimeLeft != 0 && !Instance.jumpBufferTimer.IsStopped();
+    public static bool IsHoldingJump => Input.IsActionPressed("Jump");
+
+
+    private Timer jumpBufferTimer;
+
+    public override void _EnterTree() => Instance = this;
+    public override void _ExitTree() => Instance = null;
+
+    public override void _Ready()
+    {
+        ProcessMode = ProcessModeEnum.Always;
+
+        jumpBufferTimer = new()
+        {
+            OneShot = true,
+            WaitTime = JumpBufferTime
+        };
+        AddChild(jumpBufferTimer);
+
+        JumpPressed += StartJumpBuffer;
+    }
+
+    private void StartJumpBuffer() => jumpBufferTimer.Start();
+
+    public static void UseJumpBuffer() => Instance.jumpBufferTimer.Stop();
+
+    public static float GetPlayerHorizontalInput() => Input.GetAxis("MoveLeft", "MoveRight");
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event.IsEcho())
+            return;
+
+        if (@event.IsAction("Jump"))
+        {
+            if (@event.IsPressed())
+            {
+                JumpPressed?.Invoke();
+                return;
+            }
+            JumpReleased?.Invoke();
+            return;
+        }
+
+        if (@event is InputEventKey key && !key.Pressed && key.Keycode == Key.F11)
+        {
+            ToggleFullscreen();
+            return;
+        }
+    }
+
+    private void ToggleFullscreen()
+    {
+        if (DisplayServer.WindowGetMode() is DisplayServer.WindowMode.Fullscreen or DisplayServer.WindowMode.ExclusiveFullscreen)
+        {
+            DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+            return;
+        }
+
+        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+    }
+}
